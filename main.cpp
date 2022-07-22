f622a77bd73ea4665e92dc14f48d3b1cda35e6db
m-esmat-m <m.esmat@aucegypt.edu>
2022-07-22 22:53:33 +0200

    “message”

--- main.cpp
+++ main.cpp
@@ -4,7 +4,12 @@
 using namespace std;
 
 int main() {
-
+int val[] = { 60, 100, 120 };
+int wt[] = { 10, 20, 30 };
+int W = 50;
+int n = sizeof(val) / sizeof(val[0]);
+cout << knapSack(W, wt, val, n);
+return 0;
 
 
 }
